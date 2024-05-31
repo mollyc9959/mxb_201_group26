9c8b7a30e3fbee265a3ccada223e10b56d23888b
Aiden Matthews <52370762+AidenMatthews1@users.noreply.github.com>
2024-05-31 11:25:51 +1000

    Part 1 Linear Regression
    Created D from linear regression and decomposed D into eigenvalues and eigenvectors

--- MXB201_2024_Part_I_template.m
+++ MXB201_2024_Part_I_template.m
@@ -13,6 +13,11 @@ FA  = nan(X, Y);    % fractional anistropy
 PDD = nan(X, Y, 3); % principal diffusion direction
 
 % Any other initialisation needed here
+% Removing any negative values in the data
+S0 = max(S0, 0);
+S = max(S, 0);
+A = [g(:,1).^2  g(:,2).^2  g(:,3).^2  2.*g(:,1).*g(:,2)  2.*g(:,1).*g(:,3)  2.*g(:,2).*g(:,3)];
+D = zeros(3,3);
 
 %% Compute the diffusion tensor for each pixel
 for x = 1:X
@@ -20,12 +25,33 @@ for x = 1:X
 
         % If not within the mask, skip the pixel
         if ~mask(x, y), continue; end
-
-        % Handling bad data
+        
         % Solving least squares problem
+        B = log(S(x,y,:)/S0(x,y));
+        % B is 1x1x64 but should be 1x64 
+        B = squeeze(B);        
+        
+        % Returns 1x6 matrix of important parts of D 
+        D_i = B\A;
+        
+        
+
+        
         % Forming diffusion tensor
+
+        % Manually constructing D from important parts stored in D_i
+        D(1,1) =  D_i(1,1); D(1,2) =  D_i(1,4); D(1,3) =  D_i(1,4);
+        D(2,1) =  D_i(1,4); D(2,2) =  D_i(1,2); D(2,3) = D_i(1,6);
+        D(3,1) =  D_i(1,5); D(3,2) =  D_i(1,6); D(3,3) =  D_i(1,3);
+
+        
         % Finding eigenvalues and eigenvectors
+        [D_e,D_v] = Eig(D)
+        
+
         % Calculating MD, FA and PDD
+
+
 
     end
 end
